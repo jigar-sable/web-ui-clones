6e62a852761dee19cf5df0748e48bf21f3c11ad0
Jigar Sable <jigarsable21@gmail.com>
2021-08-20 17:05:17 +0530

    favicon added

--- YouTube/public/script.js
+++ YouTube/public/script.js
@@ -12,8 +12,8 @@ fetch("videos.json")
         <!-- thumbnail -->
         <a href="${video.videoURL}">
             <div class="relative h-48 sm:h-36 w-full">
-            <img class="relative h-full w-full object-cover" src="${video.thumbnail}" alt="">
-            <span class="absolute right-1 bottom-1 p-0.5 px-1 rounded-sm bg-black text-white text-xs bg-opacity-70">38:38</span>
+            <img class="relative h-full w-full object-cover" src="${video.thumbnail}" alt="" draggable="false">
+            <span class="absolute right-1 bottom-1 p-0.5 px-1 rounded-sm bg-black text-white text-xs bg-opacity-70">${video.duration}</span>
             </div>
         </a>
         <!-- video details -->
@@ -21,7 +21,7 @@ fetch("videos.json")
 
         <div class="flex items-center space-x-3">
             <!-- channel logo -->
-            <a href="${video.desc.channelURL}" class="rounded-full w-12 sm:w-14"><img class="w-full h-full rounded-full object-cover" src="${video.desc.channelLogo}" alt=""></a>
+            <a href="${video.desc.channelURL}" class="rounded-full w-12 sm:w-14"><img class="w-full h-full rounded-full object-cover" src="${video.desc.channelLogo}" alt="" draggable="false"></a>
             <div class="flex items-start">
             <a href="${video.videoURL}" class="text-sm text-bold text-white pr-1">${video.desc.title}</a>
             <i class="material-icons text-white md-21">more_vert</i>
@@ -32,8 +32,8 @@ fetch("videos.json")
         <div class="flex flex-col pl-1 sm:pl-0 ml-12">
             <a href="${video.desc.channelURL}" class="text-gray-400 text-sm hover:text-white">${video.desc.channelName}</a>
             <div class="flex space-x-1">
-                <span class="text-sm text-gray-400">${video.desc.views}</span>
-                <span class="text-sm text-gray-400">• ${video.desc.published}</span>
+                <span class="text-sm text-gray-400">${video.desc.views} views</span>
+                <span class="text-sm text-gray-400">• ${video.desc.published} ago</span>
             </div>
         </div>
         </div>
